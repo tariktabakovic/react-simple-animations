af17882ad0b6667027ce8f8f9a37999453211597
tariktabakovic <tariktab97@gmail.com>
2020-05-23 15:20:52 -0400

    imports component1 and implements it onto page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
+import Component1 from './components/Component1';
 import './App.css';
 
 class App extends Component {
   render(){
     return (
       <div className= "App">
-        <h1>Welcome</h1>
+        <Component1></Component1>
       </div>
     );
   }
