e2d8f1d5ab5ca3bd75fe12a5173ac1a63d9f45e9
tariktabakovic <tariktab97@gmail.com>
2020-05-23 15:27:46 -0400

    implements react-spring with props

--- src/components/Component1.js
+++ src/components/Component1.js
@@ -1,14 +1,22 @@
-import React, { Component } from 'react'
-import {Spring} from 'react-spring';
-export default class Component1 extends Component {
-    render() {
+import React, { Component } from 'react';
+import { Spring } from 'react-spring/renderprops';
+
+export default function  Component1(){
         return (
-            <div style={c1Style}>
-                <h1>Component 1</h1>
-                <p>Lorem ipsum dolor sit</p> 
-            </div>
+            <Spring
+            from ={{ opacity: 0 }}
+            to ={{ opacity: 1 }}
+            >
+                {props => (
+                    <div style={props}>
+                        <div style={c1Style}>
+                            <h1>Component 1</h1>
+                            <p>Lorem ipsum dolor sit</p> 
+                        </div>
+                    </div>
+                )}
+            </Spring>
         )
-    }
 }
 
 const c1Style = {
