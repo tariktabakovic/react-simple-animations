978f44a43f8510229305db052a14cd99e064a146
tariktabakovic <tariktab97@gmail.com>
2020-05-23 15:21:57 -0400

    adds style to component1

--- src/components/Component1.js
+++ src/components/Component1.js
@@ -3,10 +3,16 @@ import React, { Component } from 'react'
 export default class Component1 extends Component {
     render() {
         return (
-            <div>
+            <div style={c1Style}>
                 <h1>Component 1</h1>
                 <p>Lorem ipsum dolor sit</p> 
             </div>
         )
     }
+}
+
+const c1Style = {
+    background: 'steelblue',
+    color: 'white',
+    padding: '1.5rem'
 }
