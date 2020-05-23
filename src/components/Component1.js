44c701c664040a63325f214dea153b968ea58201
tariktabakovic <tariktab97@gmail.com>
2020-05-23 15:28:52 -0400

    adds marginTop to Spring

--- src/components/Component1.js
+++ src/components/Component1.js
@@ -4,8 +4,8 @@ import { Spring } from 'react-spring/renderprops';
 export default function  Component1(){
         return (
             <Spring
-            from ={{ opacity: 0 }}
-            to ={{ opacity: 1 }}
+            from ={{ opacity: 0, marginTop: -500}}
+            to ={{ opacity: 1, marginTop: 0 }}
             >
                 {props => (
                     <div style={props}>
