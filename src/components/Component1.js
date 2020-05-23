7a7dd26dcad6e3a4c57e3a4f06ae1ec74b275f72
tariktabakovic <tariktab97@gmail.com>
2020-05-23 15:19:53 -0400

    creates new component with header

---
+++ src/components/Component1.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Component1 extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Component 1</h1>
+                <p>Lorem ipsum dolor sit</p> 
+            </div>
+        )
+    }
+}
