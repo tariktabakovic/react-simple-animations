c37d214b8ccc662c2b9208bd8b5abcd41b44c93c
tariktabakovic <tariktab97@gmail.com>
2020-05-25 13:00:39 -0400

    implements counter in component1 and gives it styling

--- src/components/Component1.js
+++ src/components/Component1.js
@@ -12,6 +12,18 @@ export default function  Component1(){
                         <div style={c1Style}>
                             <h1>Component 1</h1>
                             <p>Lorem ipsum dolor sit</p> 
+                            <Spring
+                                from={{number: 0}}
+                                to={{number: 10}}
+                            >
+                                {props=>(
+                                    <div style={props}>
+                                        <h1 style={counter}>
+                                            {props.number}
+                                        </h1>
+                                    </div>
+                                )}
+                            </Spring>
                         </div>
                     </div>
                 )}
@@ -24,3 +36,11 @@ const c1Style = {
     color: 'white',
     padding: '1.5rem'
 }
+
+const counter= {
+    background: '#333',
+    textAlign: 'center',
+    width: '100px',
+    borderRadius: '50%',
+    margin: '1rem auto'
+};
