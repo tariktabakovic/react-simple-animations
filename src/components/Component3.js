316c2d04ad110682012fc28f3ec7339424ab166d
tariktabakovic <tariktab97@gmail.com>
2020-05-25 13:06:35 -0400

    creates component3 and styles it

---
+++ src/components/Component3.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function Component3(){
+    return (
+        <div style={c3Style}>
+            <h1>Component 3</h1>
+            <p>fadsfasdf sadfiojsadiofj dsifjaosidf 
+                asdf oasidjf aosdifj sadoifjasjcv lxzkcvj zxclkv
+                asdfjasflkas dj akasdlf jadflj 
+            </p>
+        </div>
+    )
+}
+
+const c3Style = {
+    background: 'skyblue',
+    color: 'white',
+    padding: '1.5rem 1.5rem 5rem 1.5rem'
+}
