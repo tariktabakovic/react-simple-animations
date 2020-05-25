8b5216cfc35c120fa8f054e07fb34f1b74a611e7
tariktabakovic <tariktab97@gmail.com>
2020-05-25 13:16:31 -0400

    implements react-spring transition and animation for component3

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { Transition, animated } from 'react-spring/renderprops';
 import Component1 from './components/Component1';
 import Component2 from './components/Component2';
 import Component3 from './components/Component3';
@@ -18,7 +19,19 @@ class App extends Component {
       <div className= "App">
         <Component1 />
         <Component2 toggle={this.toggle}/>
-        <Component3 />
+        <Transition 
+          native
+          items={this.state.showComponent3}
+          from={{opacity: 0}}
+          enter={{opacity: 1}}
+          leave={{opacity: 0}}
+        >
+          {show => show && (props => (
+            <animated.div style={props}>
+              <Component3/>
+            </animated.div>
+          ))}
+        </Transition>
       </div>
     );
   }
