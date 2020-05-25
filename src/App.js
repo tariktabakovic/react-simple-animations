13fad35f2275d72b94140cf3f30bdabb9423db18
tariktabakovic <tariktab97@gmail.com>
2020-05-25 13:07:26 -0400

    brings component3 into App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, {Component} from 'react';
 import Component1 from './components/Component1';
 import Component2 from './components/Component2';
+import Component3 from './components/Component3';
 
 import './App.css';
 
 class App extends Component {
+  state = {
+    showComponent3: false
+  }
+
   render(){
     return (
       <div className= "App">
-        <Component1></Component1>
-        <Component2></Component2>
+        <Component1 />
+        <Component2 />
+        <Component3 />
       </div>
     );
   }
